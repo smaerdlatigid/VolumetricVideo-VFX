9f9f1704529168ecb0e03d24451614b8b1755cac
Kyle Pearson <pearsonk@email.arizona.edu>
2020-03-09 22:07:26 -0700

    v1

---
+++ Assets/cameraCruise.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraCruise : MonoBehaviour
+{
+    public GameObject focus;
+    public float radius = 1f;
+    public float speed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(
+            radius*Mathf.Cos(Time.time*speed),
+            0,
+            radius*Mathf.Sin(Time.time*speed)
+        );
+        transform.LookAt(focus.transform);
+
+    }
+}
